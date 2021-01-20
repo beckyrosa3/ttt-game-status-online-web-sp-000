6ff22396c46d8532f77ce18d6ed8bbdc246fa482
Becky Rosa <becky_m_rosa@yahoo.com>
2021-01-20 03:00:46 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,11 +16,14 @@ end
 ]
 
 def won?(board)
-WIN_COMBINATIONS.select do |win_combo|
-  #binding.pry
-  board(win_combo[0]) == board(win_combo[1]) &&
-  board(win_combo[1]) == board(win_combo[2]) &&
-  position_taken?(board, win_combo[0])
+  WIN_COMBINATIONS.select do |win_combo|
+    #binding.pry
+    board[win_combo[0] ] == board[win_combo[1] ] &&
+    board[win_combo[1] ] == board[win_combo[2] ] &&
+    position_taken?(board, win_combo[0])
+
+    end
+  end
 
 
   end
